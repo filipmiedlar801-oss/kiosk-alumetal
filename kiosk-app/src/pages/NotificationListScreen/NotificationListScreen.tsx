dbbe27a97af4ccb5b9fa6578117b0c0dc06de112
filipor <filipor@MacBook-Pro-filipor.local>
2026-04-21 20:58:23 +0200

    remove unused import

--- kiosk-app/src/pages/NotificationListScreen/NotificationListScreen.tsx
+++ kiosk-app/src/pages/NotificationListScreen/NotificationListScreen.tsx
@@ -12,7 +12,6 @@ import {
   DialogContent,
   DialogActions,
   CircularProgress,
-  TextField,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
